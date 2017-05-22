808da387dea7e997a3814f4f20608be00f1dace5
SCITMaster <SCITMaster@LAPTOP-71RDEFVH>
2017-05-22 14:10:44 +0900

    ClientMain 로직부분
    2017-05-22 14시 10분 [오하라]

--- Tangore/src/client/ClientMain.java
+++ Tangore/src/client/ClientMain.java
@@ -2,78 +2,105 @@ package client;
 
 import java.util.InputMismatchException;
 
-import exception.ClientManagerException;
+import exception.ManagerException;
 import vo.Tango;
 
 public class ClientMain {
 	private ClientManager manager = new ClientManager(); // 요청과 관련된 처리를 하기 위해 생성한 ClientManager 클래스의 객체
-}
-	while(true)	{
-		// 메뉴를 출력한다
-		// printMainMenu();
-
-		try {
-			// 숫자를 입력받는다
-			int no = sc.nextInt();
-
-			switch (no) {
-			// Tango 객체를 생성하여 추가한다
-			case 1:
-				// insertTango();
-				break;
-
-			// row_id를 입력받아 해당하는 Tango 객체를 찾는다
-			case 2:
-
-				// row_id를 입력받는다
-				String row_id = "";
+	
+	public ClientMain(){
+	
+		while(true)	{
+			// 메뉴를 출력한다
+			// printMainMenu();
+	
+			try {
+				// 숫자를 입력받는다
+				int no = 0;
+	
+				switch (no) {
 				
-				// 매니저 객체로부터 주민번호로 해당하는 객체를 찾는다
-				Tango find_tango = manager.(find_jumin);
-
-				break;
-
-			// 주민번호로 해당하는 Tango 객체를 삭제한다
-			case 3:
-
-				// 시퀀스 넘버를 입력받는다
-				String delete_row_id = "";
-
-				// 매니저 객체로부터 시퀀스 넘버에 해당하는 객체를 삭제를 시도하고 성공 여부를 돌려받는다
-				boolean delete_result = manager.deleteTango(delete_row_id);
-
-				if (delete_result) {
-					System.out.println("정상적으로 삭제 되었습니다.");
-				} else {
-					System.out.println("삭데할 데이터가 없습니다.");
+				// Tango 객체를 생성하여 추가한다
+				case 1:
+					// insertTango();
+					break;
+	
+				// row_id를 입력받아 해당하는 Tango 객체를 찾는다
+				case 2:	
+					// row_id를 입력받는다
+					int row_id = 0;
+					
+					// 매니저 객체로부터 시퀀스 넘버로 해당하는 객체를 찾는다
+					try {
+						Tango find_tango = manager.findTango_row_id(row_id);
+					} catch (ManagerException e1) {
+						e1.printStackTrace();
+					}
+					break;
+				// hanja를 입력받아 해당하는 Tango 객체를 찾는다		
+				case 3:
+					// hanja를 입력받는다
+					String hanja = "";
+					
+					// 매니저 객체로부터 시퀀스 넘버로 해당하는 객체를 찾는다
+					try {
+						Tango find_tango = manager.findTango_hanja(hanja);
+					} catch (ManagerException e1) {
+						e1.printStackTrace();
+					}
+					break;				
+	
+				// hiragana를 입력받아 해당하는 Tango 객체를 찾는다
+				case 4:	
+					// hiragana를 입력받는다
+					String hiragana = "";
+					
+					// 매니저 객체로부터 시퀀스 넘버로 해당하는 객체를 찾는다
+					try {
+						Tango find_tango = manager.findTango_hiragana(hiragana);
+					} catch (ManagerException e1) {
+						e1.printStackTrace();
+					}
+					break;	
+				// 현재 저장되어 있는 모든 Tango 객체를 출력한다
+				case 5:		
+					break;	
+				// 시퀀스 넘버로 해당하는 Tango 객체를 수정한다
+				case 6:				
+					break;
+				// 시퀀스 넘버로 해당하는 Tango 객체를 삭제한다
+				case 7:				
+					// 시퀀스 넘버를 입력받는다
+					int delete_row_id = 0;
+	
+					// 매니저 객체로부터 시퀀스 넘버에 해당하는 객체를 삭제를 시도하고 성공 여부를 돌려받는다
+					boolean delete_result = false;
+					try {
+						delete_result = manager.deleteTango(delete_row_id);
+					} catch (ManagerException e) {
+						e.printStackTrace();
+					}
+	
+					if (delete_result) {
+						System.out.println("정상적으로 삭제 되었습니다.");
+					} else {
+						System.out.println("삭데할 데이터가 없습니다.");
+					}
+					break;
+				// 프로그램을 종료한다
+				case 9:
+					manager.closeStreams();
+					System.exit(0);
+					break;
+	
+				// 위의 번호에 해당하지 않는다면 무시한다
+				default:
 				}
-				break;
-
-			// 현재 저장되어 있는 모든 Tango 객체를 출력한다
-			case 4:		
-				break;
-
-			case 5:				
-				break;
-			case 6:				
-				break;
-			// 프로그램을 종료한다
-			case 9:
-				manager.closeStreams();
-				System.exit(0);
-				break;
-
-			// 위의 번호에 해당하지 않는다면 무시한다
-			default:
+			} catch (InputMismatchException e) {
+				// 숫자로 입력받을 수 없는 에러가 날 경우 Exception 처리
+				// e.printStackTrace();
 			}
-		} catch (InputMismatchException e) {
-			// 숫자로 입력받을 수 없는 에러가 날 경우 Exception 처리
-			// e.printStackTrace();
-		} catch (ClientManagerException e) {
-			// 사용자 정의 익셉션이 일어날 경우 처리
-			// e.printStackTrace();
 		}
-
 	}
 
 	/**
