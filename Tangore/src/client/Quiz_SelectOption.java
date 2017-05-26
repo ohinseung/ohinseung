10e1fa5af624383af867c5d426171ef0cb33607d
SCITMaster <SCITMaster@LAPTOP-6VAT6USG>
2017-05-26 16:40:48 +0900

    2017-05-26
    이승현
    문제 개수를 선택 하는 창에서
    취소 버튼이 작동 되는 코드 추가

--- Tangore/src/client/Quiz_SelectOption.java
+++ Tangore/src/client/Quiz_SelectOption.java
@@ -5,6 +5,8 @@
 package client;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -16,10 +18,10 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class Quiz_SelectOption extends JFrame 
+public class Quiz_SelectOption extends JFrame
 {
-	private JPanel contentPane;
-	JTextField quiz_num_textField;
+	private JPanel	contentPane;
+	JTextField		quiz_num_textField;
 
 	/**
 	 * Create the frame.
@@ -27,20 +29,21 @@ public class Quiz_SelectOption extends JFrame
 
 	public boolean checkinput(String quiz_num_textField)
 	{
-		boolean result=false;
+		boolean result = false;
 		char charinput;
-		for(int i=0;i<quiz_num_textField.length();i++)
+		for (int i = 0; i < quiz_num_textField.length(); i++)
 		{
-			charinput=quiz_num_textField.charAt(i);
-			
-			if(charinput>=0x30 && charinput<=0x39)
+			charinput = quiz_num_textField.charAt(i);
+
+			if (charinput >= 0x30 && charinput <= 0x39)
 			{
-				result= true;
+				result = true;
 			}
 		}
 		return result;
 	}
-	public Quiz_SelectOption() 
+
+	public Quiz_SelectOption()
 	{
 		setTitle("문제풀이");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,73 +52,80 @@ public class Quiz_SelectOption extends JFrame
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
-		
+
 		JLabel quiz_title_label = new JLabel("문제풀이 옵션");
 		quiz_title_label.setFont(new Font("a옛날사진관3", Font.PLAIN, 25));
 		quiz_title_label.setBounds(155, 12, 152, 44);
 		contentPane.add(quiz_title_label);
-		
+
 		JLabel quiz_num_label = new JLabel("문제 개수 :");
 		quiz_num_label.setFont(new Font("a옛날사진관3", Font.PLAIN, 25));
 		quiz_num_label.setBounds(21, 71, 139, 44);
 		contentPane.add(quiz_num_label);
-		
-		quiz_num_textField= new JTextField("");
+
+		quiz_num_textField = new JTextField("");
 		quiz_num_textField.setFont(new Font("a옛날사진관3", Font.PLAIN, 20));
 		quiz_num_textField.setBounds(174, 77, 195, 36);
 		contentPane.add(quiz_num_textField);
-		
+
 		JLabel player_label = new JLabel("인       원 : ");
 		player_label.setFont(new Font("a옛날사진관3", Font.PLAIN, 25));
 		player_label.setBounds(24, 133, 130, 33);
 		contentPane.add(player_label);
-		
+
 		JRadioButton player_1_btn = new JRadioButton("1명");
 		player_1_btn.setFont(new Font("a옛날사진관3", Font.PLAIN, 25));
 		player_1_btn.setBounds(199, 133, 65, 33);
 		contentPane.add(player_1_btn);
-		
+
 		JRadioButton player_2_btn = new JRadioButton("2명");
 		player_2_btn.setFont(new Font("a옛날사진관3", Font.PLAIN, 25));
 		player_2_btn.setBounds(294, 136, 75, 26);
 		contentPane.add(player_2_btn);
-		
+
 		ButtonGroup btngroup = new ButtonGroup();
 		btngroup.add(player_1_btn);
 		btngroup.add(player_2_btn);
-		
+
 		JLabel test_subject_label = new JLabel("시험 볼 항목");
 		test_subject_label.setFont(new Font("a옛날사진관3", Font.PLAIN, 25));
 		test_subject_label.setBounds(21, 186, 146, 36);
 		contentPane.add(test_subject_label);
-		
+
 		JCheckBox hanja_ckbox = new JCheckBox("한자");
 		hanja_ckbox.setFont(new Font("a옛날사진관3", Font.PLAIN, 20));
 		hanja_ckbox.setBounds(195, 195, 69, 27);
 		contentPane.add(hanja_ckbox);
-		
+
 		JCheckBox hiragana_ckbox = new JCheckBox("히라가나");
 		hiragana_ckbox.setFont(new Font("a옛날사진관3", Font.PLAIN, 20));
 		hiragana_ckbox.setBounds(270, 195, 114, 27);
 		contentPane.add(hiragana_ckbox);
-		
+
 		JCheckBox meaning_ckbox = new JCheckBox("뜻");
 		meaning_ckbox.setFont(new Font("a옛날사진관3", Font.PLAIN, 20));
 		meaning_ckbox.setBounds(390, 195, 58, 27);
 		contentPane.add(meaning_ckbox);
-		
+
 		JButton quiz_ok_Btn = new JButton("확인");
 		quiz_ok_Btn.setFont(new Font("a옛날사진관3", Font.PLAIN, 25));
 		quiz_ok_Btn.setBounds(65, 248, 146, 44);
 		contentPane.add(quiz_ok_Btn);
-		
+
 		JButton quiz_cancel_Btn = new JButton("취소");
 		quiz_cancel_Btn.setFont(new Font("a옛날사진관3", Font.PLAIN, 25));
 		quiz_cancel_Btn.setBounds(253, 248, 146, 44);
 		contentPane.add(quiz_cancel_Btn);
-		
+		quiz_cancel_Btn.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				dispose();
+				new ClientMainUI();
+			}
+		});
+
 		setVisible(true);
 	}
 }
