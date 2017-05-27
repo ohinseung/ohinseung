868d0eb65f878b74e5abb45c33f49885b4dfc952
SCITMaster <SCITMaster@LAPTOP-6VAT6USG>
2017-05-27 15:02:20 +0900

    작성자 : 이승현
    2017-05-27
    create user_id table

---
+++ Tangore/create_user_id.sql
@@ -0,0 +1,10 @@
+--작성일 : 2017-05-27.
+/*작성자 : 오하라,김인우,이승현.
+ * 사용자 id를 보관하는 table.
+ * hr계정에서 생성했다.
+ * */
+create table user_id
+(
+nickname varchar2(20) primary key,
+status number(1) not null
+);
